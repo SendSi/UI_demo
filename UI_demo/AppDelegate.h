8983dd1cd65a4add4c688f0fd2b2c9ab1fbb9493
SendSi <scuplt@scupltdeMac.local>
2016-03-20 18:53:01 +0800

    Initial Commit

---
+++ UI_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI_demo
+//
+//  Created by scuplt on 16-3-20.
+//  Copyright (c) 2016年 Scuplt0413. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
